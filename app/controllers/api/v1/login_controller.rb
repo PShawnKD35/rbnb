2303c72e740366bb39f795c4c7d6b300cc91c659
Shawn Peng <shawn-peng@outlook.com>
2019-08-01 12:36:01 +0800

    Moved login controller to api/v1

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,4 +1,4 @@
-class LoginController < ApplicationController
+class Api::V1::LoginController < Api::V1::BaseController
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
