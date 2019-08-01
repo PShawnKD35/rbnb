c12608761bb1d57719ff8bba190f906d73aeec96
Shawn Peng <shawn-peng@outlook.com>
2019-08-01 12:38:53 +0800

    Updated routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  post '/login', to: 'login#login'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+
+      post '/login', to: 'login#login'
 
       resources :users, only: [:create, :show, :update] do
         resources :services, only: [:create]
