a06213dddcc7e514d7688fe1fd8c42006e690a5a
Shawn Peng <shawn-peng@outlook.com>
2019-08-01 10:21:56 +0800

    Added category and migration

--- app/views/api/v1/services/show.json.jbuilder
+++ app/views/api/v1/services/show.json.jbuilder
@@ -1,6 +1,6 @@
 json.service do
   # json.merge! @service
-  json.extract! @service, :id, :name, :description, :address, :latitude, :longitude, :user, :charge, :items
+  json.extract! @service, :id, :name, :description, :address, :latitude, :longitude, :user, :charge, :items, :category
   # json.items @service.items do |item|
   #   json.merge! item
   # end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_081642) do
+ActiveRecord::Schema.define(version: 2019_08_01_022045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_07_31_081642) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
