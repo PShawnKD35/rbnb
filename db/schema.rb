5a11db12451ca5b5dc9640790a2e9ca13f9b5859
Shawn Peng <shawn-peng@outlook.com>
2019-08-02 13:11:31 +0800

    Added remote_photo_url to service model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_022045) do
+ActiveRecord::Schema.define(version: 2019_08_02_051050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_08_01_022045) do
     t.float "latitude"
     t.float "longitude"
     t.string "category"
+    t.string "remote_photo_url"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
